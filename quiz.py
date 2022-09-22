463cc6f0619eafbf4b1abcaa52d6f5b45badad6d
Laura Berends <laura.berends@hotmail.com>
2022-09-22 10:16:36 +0200

    quiz.py test1

---
+++ quiz.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+# When someone throws you a ball, you are
+# a) confident you'll catch it or b) nervous you'll catch it
+
+
+
+# Can you easily touch your toes when you bend over? 
+
+# Sprint or marathon?
+
