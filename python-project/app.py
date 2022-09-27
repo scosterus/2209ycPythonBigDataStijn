0fa15eadb14a2d174fa385af8442b0ce7e7519df
MehmetHizal <mh.hizal@gmail.com>
2022-09-27 16:30:14 +0200

    nieuw

--- python-project/app.py
+++ python-project/app.py
@@ -13,21 +13,24 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-#mycursor.execute("CREATE DATABASE skillapp")
-#mycursor.execute("CREATE TABLE users (ID int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), age int(3), endurance int(11), strength int(11), power int(11), speed int(11), agility int(11), nerve int(11), durability int(11), hand_eye_co int(11), analytical_appr int(11))")
+# mycursor.execute("CREATE DATABASE skillapp")
+# mycursor.execute("CREATE TABLE users (ID int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), age int(3), endurance int(11), strength int(11), power int(11), speed int(11), agility int(11), nerve int(11), durability int(11), hand_eye_co int(11), analytical_appr int(11))")
 
-db.commit()
+# db.commit()
 
 print("Done!")
 
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/intro")
-def intro():
-    return render_template('intro.html')
+@app.route("/intro/<vnaam>/<age>")
+def intro(vnaam,age):
+    mycursor.execute("INSERT INTO users (name, age) VALUES ('%s', '%s')" % (vnaam, age))
+    db.commit()
+    print(vnaam, age, "added")
+    return "data naam en leeftijd"
 
 @app.route("/quiz")
 def quiz():
@@ -40,8 +43,3 @@ def fits():
 @app.route("/home")
 def home():
     return render_template('home.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
