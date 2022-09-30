817777d4e5ca1d85bfa0c7c1708516d7bffdffe7
MehmetHizal <mh.hizal@gmail.com>
2022-09-30 09:21:50 +0200

    vandaag

--- python-project/app.py
+++ python-project/app.py
@@ -4,37 +4,39 @@ import mysql.connector
 app = Flask(__name__)
 
 db = mysql.connector.connect(
-    host = "localhost",
-    port = "8889",
-    user = "root",
-    password = "root",
+    host = "bigdata-database.mysql.database.azure.com",
+    port = "3306",
+    user = "bigData",
+    password = "qwer1234QWER!@#$",
     database = "skillapp"
     )
 
 mycursor = db.cursor()
 
 # mycursor.execute("CREATE DATABASE skillapp")
-# mycursor.execute("CREATE TABLE users (ID int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), age int(3), endurance int(11), strength int(11), power int(11), speed int(11), agility int(11), nerve int(11), durability int(11), hand_eye_co int(11), analytical_appr int(11))")
+# mycursor.execute("CREATE TABLE users (ID int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), age int(3), endurance int(11), strength int(11), power int(11), speed int(11), flexibilty int(11), agility int(11), nerve int(11), durability int(11), hand_eye_co int(11), analytical_appr int(11))")
 
 # db.commit()
 
 print("Done!")
 
 
-@app.route("/", methods = ['POST', 'GET'])
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return "hallo"
 
 @app.route("/intro/<vnaam>/<age>")
 def intro(vnaam,age):
     mycursor.execute("INSERT INTO users (name, age) VALUES ('%s', '%s')" % (vnaam, age))
-    db.commit()
     print(vnaam, age, "added")
     return "data naam en leeftijd"
 
-@app.route("/quiz")
-def quiz():
-    return render_template('quiz.html')
+@app.route("/quiz/<speed>")
+def quiz(speed):
+    mycursor.execute("UPDATE users SET speed = '%s' WHERE ID = (SELECT MAX(ID))" % (speed))
+    db.commit()
+    print(speed, "added")
+    return "beantwoord speed"
 
 @app.route("/fits")
 def fits():
